f8c62a4439ae1c1db17af12dce9ab6c6865ee8a6
J.A. Higgs <Joe@Higgs-Tx.com>
2023-12-30 17:10:35 -0600

    etradeOptionQuote

--- src/main/java/com/hpi/entities/OpenOptionFIFOModel.java
+++ src/main/java/com/hpi/entities/OpenOptionFIFOModel.java
@@ -3,22 +3,25 @@ package com.hpi.entities;
 import lombok.*;
 
 @Getter @Setter @Builder
-public class OpenOptionFIFOModel {
+public class OpenOptionFIFOModel
+{
 //    public static final String UPDATE_MKTVAL = "update hlhtxc5_dmOfx.OpenOptionFIFO as osf, hlhtxc5_dmOfx.Util_LastDailyOption as lds set MktVal = Price * Units * ShPerCtrct where osf.EquityId = lds.EquityId and JoomlaId = '%s';";
 //    
 //    public static final String CLEAR_MKTVAL = "update hlhtxc5_dmOfx.OpenOptionFIFO set MktVal = 0.0 where JoomlaId = '%s';";
-    
-    public static final String INSERT_ALL_VALUES = "insert into hlhtxc5_dmOfx.OpenOptionFIFO (%s) values ";
-    
-//    public static final String UPDATE_GAIN = "update hlhtxc5_dmOfx.OpenOptionFIFO, hlhtxc5_dmOfx.Util_LastDailyOption set OpenOptionFIFO.GainPct = if (Units < 0, -round(100 * (Util_LastDailyOption.Price - OpenOptionFIFO.PriceOpen)/OpenOptionFIFO.PriceOpen, 4), round(100 * (Util_LastDailyOption.Price - OpenOptionFIFO.PriceOpen)/OpenOptionFIFO.PriceOpen, 4)) where Util_LastDailyOption.EquityId = OpenOptionFIFO.EquityId and OpenOptionFIFO.JoomlaId ='%s'";
-    
-    public static final String ALL_FIELDS =
-        "DMAcctId, JoomlaId, FiTId, Ticker, EquityId, TransactionName, OptionType, StrikePrice, DateOpen, DateClose, DateExpire, ShPerCtrct, Units, PriceOpen, PriceClose, MarkUpDn, Commission, Taxes, Fees, TransLoad, TotalOpen, TotalClose, CurSym, SubAcctSec, SubAcctFund, TransactionType, ReversalFiTId, Complete, OpeningOpen, OpeningHigh, OpeningLow, OpeningClose, Days";
 
-    public static final String DEMO_FIELDS =
-        "FiTId, Ticker, EquityId, TransactionName, OptionType, StrikePrice, DateOpen, DateClose, DateExpire, ShPerCtrct, Units, PriceOpen, PriceClose, MarkUpDn, Commission, Taxes, Fees, TransLoad, TotalOpen, TotalClose, CurSym, SubAcctSec, SubAcctFund, TransactionType, ReversalFiTId, Complete, OpeningOpen, OpeningHigh, OpeningLow, OpeningClose, Days";
-    
-public static final String insertAll(OpeningOptionModel oo, Integer userId) {
+    public static final String INSERT_ALL_VALUES = "insert into hlhtxc5_dmOfx.OpenOptionFIFO (%s) values ";
+
+//    public static final String UPDATE_GAIN = "update hlhtxc5_dmOfx.OpenOptionFIFO, hlhtxc5_dmOfx.Util_LastDailyOption set OpenOptionFIFO.GainPct = if (Units < 0, -round(100 * (Util_LastDailyOption.Price - OpenOptionFIFO.PriceOpen)/OpenOptionFIFO.PriceOpen, 4), round(100 * (Util_LastDailyOption.Price - OpenOptionFIFO.PriceOpen)/OpenOptionFIFO.PriceOpen, 4)) where Util_LastDailyOption.EquityId = OpenOptionFIFO.EquityId and OpenOptionFIFO.JoomlaId ='%s'";
+    public static final String ALL_FIELDS
+        = "DMAcctId, JoomlaId, FiTId, Ticker, EquityId, TransactionName, OptionType, StrikePrice, DateOpen, DateClose, DateExpire, ShPerCtrct, Units, PriceOpen, PriceClose, MarkUpDn, Commission, Taxes, Fees, TransLoad, TotalOpen, TotalClose, CurSym, SubAcctSec, SubAcctFund, TransactionType, ReversalFiTId, Complete, OpeningOpen, OpeningHigh, OpeningLow, OpeningClose, Days";
+
+    public static final String DEMO_FIELDS
+        = "FiTId, Ticker, EquityId, TransactionName, OptionType, StrikePrice, DateOpen, DateClose, DateExpire, ShPerCtrct, Units, PriceOpen, PriceClose, MarkUpDn, Commission, Taxes, Fees, TransLoad, TotalOpen, TotalClose, CurSym, SubAcctSec, SubAcctFund, TransactionType, ReversalFiTId, Complete, OpeningOpen, OpeningHigh, OpeningLow, OpeningClose, Days";
+
+    public static final String ACTIVE_OPTIONS = "select EquityId from hlhtxc5_dmOfx.OpenOptionFIFO oof where oof.DateExpire >= curdate();";
+
+    public static final String insertAll(OpeningOptionModel oo, Integer userId)
+    {
         StringBuilder sb;
 
         sb = new StringBuilder();
